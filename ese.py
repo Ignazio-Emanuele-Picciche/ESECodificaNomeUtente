6bcef7653ba5352dfb54040803d7cd8591832124
Ignazio-Emanuele-Picciche <82161529+Ignazio-Emanuele-Picciche@users.noreply.github.com>
2023-10-29 12:04:30 +0100

    migliorato il salvataggio del nuovo file

--- ese.py
+++ ese.py
@@ -1,59 +1,39 @@
 import json
-from ast import literal_eval
 import numpy as np
 
 # mi creo una lista di tuple con i nomi e la seguenza di cifre associate
-def modlist(lung_logs, new_list):
-    for x in my_set:
-        rand=np.random.randint(10000000, 100000000)
-        my_list.append([x, rand])
+def modlist():
+    for x in set_nomi:
+        rand=np.random.randint(10000000, 100000000) # Genero casualmente un numero (codice identificati) che andrà associato univocamente ad un nome
+        nome_codice.append([x, rand])   # aggiungo la coppia nome - codice identificativo all'array nome_codice
         for i in range(lung_logs):
-            if new_list[i][1] == x:
-                new_list[i][1] = rand
-    return new_list
+            if lista_log[i][1] == x:
+                lista_log[i][1] = rand
 
 
-fin = open('C:\\Users\\ignaz\\Desktop\\UCBM\\Programmazione\\ESECodificaNomeUtente\\logs_anonimizzato_1.json')
+fin = open('logs_anonimizzato_1.json')
+lista_log = json.load(fin)
 
-lista_persone = json.load(fin)
+lung_logs = len(lista_log)
 
-lista_json = json.dumps(lista_persone, indent=4)
-new_list=np.array(literal_eval(lista_json)).tolist()
-
-
-lung_logs = len(new_list)
-# lung_log = len(new_list[0])
-
-my_set = set()
-my_list = []
+set_nomi = set()  # Creo un set per salvare i nomi in maniera univoca
+nome_codice = []  # Creo una lista dove andare a slvare le coppie nome - codice identificativo
 
 # mi creo il set con tutti i nomi (set di nomi univoci dato che è un set :) )
 # nel mentre mi elimino anche il terzo campo, come richiesto dalla traccia
 for i in range(lung_logs):
-    my_set.add(new_list[i][1])
-    new_list[i].pop(2)
+    set_nomi.add(lista_log[i][1])
+    lista_log[i].pop(2)
 
-modlist(lung_logs, new_list)
-print(new_list)
-
-
-fin.close()
-
-# Salvo i log modificati in un nuovo file (cosi non tocco il file originale)
-fin = open('C:\\Users\\ignaz\\Desktop\\UCBM\\Programmazione\\ESECodificaNomeUtente\\logs_anonimizzato_1_new.json','w')
-
-fin.write('[')
-for x in new_list:
-    fin.write('\n'+str(x)+',')
-
-fin.write('\n'+']')
+modlist()
 fin.close()
 
 
-
-# Stampo la lista di tuple per vedere come sono codificati i valori
-for x in my_list:
-    print(x)
-
+lista_log = json.dumps(lista_log, indent=4)
+with open('logs_anonimizzato_1_new.json','w')  as of:
+    of.write(lista_log)
 
 
+nome_codice = json.dumps(nome_codice, indent=1)
+with open('nome_codice.json', 'w') as f:
+    f.write(nome_codice)
